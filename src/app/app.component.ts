6d86909c772cf3d263d4aec0d43f020849865d87
Ayoub Ajdour <ayoubajdour20@gmail.com>
2024-03-02 12:48:28 +0100

    adding @Input() & @Output()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet ,RouterModule} from '@angular/router';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RouterModule,CommonModule,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'testfdar';
+  static raqm(arg0: number) {
+    throw new Error('Method not implemented.');
+  }
+  title = 'BioToudert';
+  private _raqm: number=0;
+  public get raqm(): number {
+    return this._raqm;
+  }
+  public set raqm(value: number) {
+    this._raqm = value;
+  }
 }
